2e04f8a1e2270d78155d5df64beb658327b6cc3f
Sergio Clemente <sergiotucano@yahoo.com.br>
2024-11-27 14:54:25 -0300

    remove win_ble

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -7,11 +7,8 @@
 #include "generated_plugin_registrant.h"
 
 #include <flutter_libserialport/flutter_libserialport_plugin.h>
-#include <win_ble/win_ble_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   FlutterLibserialportPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FlutterLibserialportPlugin"));
-  WinBlePluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("WinBlePlugin"));
 }
